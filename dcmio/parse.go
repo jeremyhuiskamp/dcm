562a3c89c9e4e0696818fe10687a5131d332b329
Jeremy Huiskamp <jeremy.huiskamp@karoshealth.com>
2013-09-09 01:07:04 -0400

    switch transfer syntaxes when finished group 2

--- dcmio/parse.go
+++ dcmio/parse.go
@@ -2,12 +2,15 @@ package dcmio
 
 import (
     bin "encoding/binary"
+    "bytes"
     "github.com/kamper/dcm/dcm"
     "errors"
     "io"
     "io/ioutil"
 )
 
+// positionReader wraps another io.Reader and counts the
+// number of bytes that have been read
 type positionReader struct {
     position uint64
     in io.Reader
@@ -19,6 +22,8 @@ func (pr *positionReader) Read(p []byte) (n int, err error) {
     return
 }
 
+// DICOM files consist of a list of Tags
+// TODO: This would probably better be called an Element...
 type Tag struct {
     // offset of beginning of header from start of stream
     Offset      uint64
@@ -31,26 +36,35 @@ type Tag struct {
     Value       io.Reader
 }
 
-type Parser struct {
-    basein *positionReader
-    in io.Reader
-    // these kind of imitate the transfer syntax:
-    order bin.ByteOrder
-    explicitVR bool
+type Parser interface {
+    // Returns the current number of bytes read from the
+    // start of the stream
+    GetPosition() uint64
 
-    Preamble [128]byte
-
-    previousTag *Tag
-
-    // TODO: store sequence / item stack...
-    // TODO: remember stream position
+    // Reads the next tag from the stream.
+    // Returns (nil, nil) if EOF is reached gracefully
+    NextTag() (*Tag, error)
 }
 
-func (p *Parser) GetPosition() uint64 {
+type SimpleParser struct {
+    basein      *positionReader
+    in          io.Reader
+
+    // These kind of imitate the transfer syntax.
+    // TODO: replace with ts api
+    order       bin.ByteOrder
+    explicitVR  bool
+
+    // track the previous tag we returned, so that we can
+    // make sure it's stream is drained
+    previousTag *Tag
+}
+
+func (p *SimpleParser) GetPosition() uint64 {
     return p.basein.position
 }
 
-func (p *Parser) readTag() (tag *Tag, err error) {
+func (p *SimpleParser) readTag() (tag *Tag, err error) {
     offset := p.GetPosition()
 
     var bytes [4]byte
@@ -72,7 +86,7 @@ func (p *Parser) readTag() (tag *Tag, err error) {
     return tag, nil
 }
 
-func (p *Parser) skipPreviousValue() (err error) {
+func (p *SimpleParser) skipPreviousValue() (err error) {
     if p.previousTag != nil {
         _, err = io.Copy(ioutil.Discard, p.previousTag.Value)
     }
@@ -80,13 +94,11 @@ func (p *Parser) skipPreviousValue() (err error) {
 }
 
 // Returns (nil, nil) if there are no more tags.
-func (p *Parser) NextTag() (tag *Tag, err error) {
+func (p *SimpleParser) NextTag() (tag *Tag, err error) {
     err = p.skipPreviousValue()
     if err != nil {
         return nil, err
     }
-
-    // TODO: detect when finished file meta info and switch ts
 
     tag, err = p.readTag()
     if tag == nil {
@@ -137,60 +149,160 @@ func (p *Parser) NextTag() (tag *Tag, err error) {
     return tag, nil
 }
 
-// Construct a new Parser for a part-10 file
-func NewFileParser(in io.Reader) (p Parser, err error) {
-    basein := positionReader{position: 0, in: in}
-    p = Parser{
-        basein: &basein,
-        in: &basein,
-        // these are the defaults for group 2:
-        order: bin.LittleEndian,
-        explicitVR: true,
+type part10state int
+const (
+    beforeGroup2 = iota
+    inGroup2
+    pastGroup2
+)
+
+type Part10Parser struct {
+    basein      *positionReader
+    parser      SimpleParser
+    state       part10state
+    ts          dcm.TransferSyntax
+}
+
+func (p *Part10Parser) GetPosition() uint64 {
+    return p.basein.position
+}
+
+func (p* Part10Parser) NextTag() (tag *Tag, err error) {
+    switch p.state {
+    case beforeGroup2:
+        tag, err = p.parser.NextTag()
+        if err != nil {
+            return nil, err
+        }
+
+        if tag == nil {
+            return nil, nil
+        }
+
+        if tag.Group == 0x0002 && tag.Tag == 0x0000 {
+            p.state = inGroup2
+
+            buf, err := bufferValue(tag)
+            if err != nil {
+                return nil, err
+            }
+
+            // TODO: validate VR and VL?
+            var fmiLength uint32
+            bin.Read(&buf, p.ts.ByteOrder(), &fmiLength)
+
+            p.parser = SimpleParser{
+                basein:     p.basein,
+                in:         io.LimitReader(p.basein, int64(fmiLength)),
+                order:      p.ts.ByteOrder(),
+                explicitVR: p.ts.VR() == dcm.Explicit,
+            }
+        }
+
+        return tag, nil
+
+    case inGroup2:
+        tag, err = p.parser.NextTag()
+        if err != nil {
+            return nil, err
+        }
+
+        if tag == nil {
+            p.state = pastGroup2
+
+            if p.ts == nil {
+                // shouldn't happen, but we'll keep
+                // going with the group2 default
+                p.ts = dcm.ExplicitVRLittleEndian
+            }
+
+            p.parser = SimpleParser{
+                basein:     p.basein,
+                in:         p.basein,
+                order:      p.ts.ByteOrder(),
+                explicitVR: p.ts.VR() == dcm.Explicit,
+            }
+
+            // recurse:
+            return p.NextTag()
+        }
+
+        if tag.Tag == 0x0010 {
+            buf, err := bufferValue(tag)
+            if err != nil {
+                return tag, err
+            }
+
+            // TODO: learn how buf.String() handles charsets
+            p.ts = dcm.GetTransferSyntax(buf.String())
+        }
+
+        return tag, nil
+
+    case pastGroup2:
+        fallthrough
+    default:
+        return p.parser.NextTag()
+    }
+}
+
+// Reads the tag value into a buffer
+func bufferValue(tag *Tag) (buf bytes.Buffer, err error) {
+    _, err = buf.ReadFrom(tag.Value)
+    if err != nil {
+        return
     }
 
+    // make a copy so the tag can still be read:
+    tag.Value = bytes.NewBuffer(buf.Bytes())
+    return
+}
+
+// Construct a new Parser for a part-10 file
+func NewFileParser(in io.Reader) (Parser, error) {
+    basein := positionReader{position: 0, in: in}
+
     var preamble [132]byte
-    numRead, err := io.ReadFull(p.in, preamble[:])
+    numRead, err := io.ReadFull(&basein, preamble[:])
     if numRead < len(preamble) {
-        return p, io.EOF
+        return nil, io.EOF
     }
 
     if err != nil {
-        return p, err
+        return nil, err
     }
-
-    //p.Preamble = preamble[0:128]
 
     dicm := preamble[128:132]
     if !(dicm[0] == 'D' && dicm[1] == 'I' && dicm[2] == 'C' && dicm[3] == 'M') {
-        return p, errors.New("Not a Part10 file")
+        return nil, errors.New("Not a Part10 file")
     }
 
-    // TODO: we should actually deliver group length to the caller...
-    tag, err := p.NextTag()
-    if err != nil {
-        return p, err
-    }
+    defTS := dcm.ExplicitVRLittleEndian
 
-    if tag.Group != 0x0002 || tag.Tag != 0x0000 {
-        return p, errors.New("Missing File Meta-Information Group Length")
+    p := &Part10Parser{
+        basein:     &basein,
+        parser:     SimpleParser{
+                        basein:     &basein,
+                        in:         &basein,
+                        order:      defTS.ByteOrder(),
+                        explicitVR: defTS.VR() == dcm.Explicit,
+                    },
+        ts:         defTS,
+        state:      beforeGroup2,
     }
-
-    // TODO: ensure VR=UL
-    var fmiLength uint32
-    err = bin.Read(tag.Value, p.order, &fmiLength)
-    if err != nil {
-        return p, err
-    }
-
-    // this actually limits us to reading the fmi, instead we should be
-    // providing a way to switch over to the data in a different transfer
-    // syntax
-    p.in = io.LimitReader(p.in, int64(fmiLength))
 
     return p, nil
 }
 
 // Construct a new Parser for a stream without a part-10 header
 func NewStreamParser(in io.Reader, order bin.ByteOrder, explicitVR bool) Parser {
-    return Parser{ in: in, order: order, explicitVR: explicitVR }
+    basein := positionReader{position: 0, in: in}
+
+    return &SimpleParser{
+        basein: &basein,
+        in: &basein,
+        order: order,
+        explicitVR:
+        explicitVR,
+    }
 }
