594a6dfffbcaef2182e3bea579902a9dba042a7b
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2017-06-07 09:26:10 +0200

    remove gomega

--- dcmnet/assoc_test.go
+++ dcmnet/assoc_test.go
@@ -2,71 +2,74 @@ package dcmnet
 
 import (
 	"bytes"
-	"github.com/jeremyhuiskamp/dcm/dcm"
-	. "github.com/onsi/gomega"
 	"io/ioutil"
+	"reflect"
 	"testing"
+
+	"github.com/jeremyhuiskamp/dcm/dcm"
 )
 
 func TestParseAssocRQCEcho(t *testing.T) {
-	RegisterTestingT(t)
+	exp := AssociateRQAC{
+		ProtocolVersion:    1,
+		CalledAE:           "RIALTO",
+		CallingAE:          "DCMECHO",
+		ApplicationContext: "1.2.840.10008.3.1.1.1",
+		PresentationContexts: []PresentationContext{{
+			ID:             PCID(1),
+			Result:         PCAcceptance,
+			AbstractSyntax: "1.2.840.10008.1.1",
+			TransferSyntaxes: []dcm.TransferSyntax{
+				dcm.ImplicitVRLittleEndian,
+			},
+		}},
+		MaxPDULength:           16384,
+		ImplementationClassUID: "1.2.40.0.13.1.1",
+		ImplementationVersion:  "dcm4che-2.0",
+		// hmm, not a good test, since these are the defaults:
+		MaxOperationsInvoked:   0,
+		MaxOperationsPerformed: 0,
+	}
+	got := readAssocRQAC(t, "testdata/assocrq_cecho.bin", PDUAssociateRQ)
 
-	assocrq := readAssocRQAC("testdata/assocrq_cecho.bin", PDUAssociateRQ)
-
-	Expect(assocrq.ProtocolVersion).To(Equal(int16(1)))
-	Expect(assocrq.CalledAE).To(Equal("RIALTO"))
-	Expect(assocrq.CallingAE).To(Equal("DCMECHO"))
-	Expect(assocrq.ApplicationContext).To(Equal("1.2.840.10008.3.1.1.1"))
-
-	Expect(assocrq.PresentationContexts).To(HaveLen(1))
-	pc1 := assocrq.PresentationContexts[0]
-	Expect(pc1.ID).To(Equal(PCID(1)))
-	Expect(pc1.AbstractSyntax).To(Equal("1.2.840.10008.1.1"))
-	Expect(pc1.Result).To(Equal(PCAcceptance)) // should always be 0 in a request
-	Expect(pc1.TransferSyntaxes).To(HaveLen(1))
-	Expect(pc1.TransferSyntaxes[0]).To(Equal(dcm.ImplicitVRLittleEndian))
-
-	Expect(assocrq.MaxPDULength).To(Equal(uint32(16384)))
-	Expect(assocrq.ImplementationClassUID).To(Equal("1.2.40.0.13.1.1"))
-
-	// hmm, not a good test, since these are the defaults:
-	Expect(assocrq.MaxOperationsInvoked).To(Equal(uint16(0)))
-	Expect(assocrq.MaxOperationsPerformed).To(Equal(uint16(0)))
-
-	Expect(assocrq.ImplementationVersion).To(Equal("dcm4che-2.0"))
+	if !reflect.DeepEqual(exp, got) {
+		t.Fatalf("expected\n%+V\ngot\n%+V", exp, got)
+	}
 }
 
 func TestParseAssocACCEcho(t *testing.T) {
-	RegisterTestingT(t)
+	exp := AssociateRQAC{
+		ProtocolVersion:    1,
+		CalledAE:           "RIALTO",
+		CallingAE:          "DCMECHO",
+		ApplicationContext: "1.2.840.10008.3.1.1.1",
+		PresentationContexts: []PresentationContext{{
+			ID:             1,
+			Result:         PCAcceptance,
+			AbstractSyntax: "",
+			TransferSyntaxes: []dcm.TransferSyntax{
+				dcm.ImplicitVRLittleEndian,
+			},
+		}},
+		MaxPDULength:           16384,
+		ImplementationClassUID: "1.2.40.0.13.1.1",
+		ImplementationVersion:  "dcm4che-2.0",
+		// hmm, not a good test, since these are the defaults:
+		MaxOperationsInvoked:   0,
+		MaxOperationsPerformed: 0,
+	}
+	got := readAssocRQAC(t, "testdata/assocac_cecho.bin", PDUAssociateAC)
 
-	assocac := readAssocRQAC("testdata/assocac_cecho.bin", PDUAssociateAC)
-
-	Expect(assocac.ProtocolVersion).To(Equal(int16(1)))
-	Expect(assocac.CalledAE).To(Equal("RIALTO"))
-	Expect(assocac.CallingAE).To(Equal("DCMECHO"))
-	Expect(assocac.ApplicationContext).To(Equal("1.2.840.10008.3.1.1.1"))
-
-	Expect(assocac.PresentationContexts).To(HaveLen(1))
-	pc1 := assocac.PresentationContexts[0]
-	Expect(pc1.ID).To(Equal(PCID(1)))
-	Expect(pc1.AbstractSyntax).To(HaveLen(0))
-	Expect(pc1.Result).To(Equal(PCAcceptance))
-	Expect(pc1.TransferSyntaxes).To(HaveLen(1))
-	Expect(pc1.TransferSyntaxes[0]).To(Equal(dcm.ImplicitVRLittleEndian))
-
-	Expect(assocac.MaxPDULength).To(Equal(uint32(16384)))
-	Expect(assocac.ImplementationClassUID).To(Equal("1.2.40.0.13.1.1"))
-
-	// hmm, not a good test, since these are the defaults:
-	Expect(assocac.MaxOperationsInvoked).To(Equal(uint16(0)))
-	Expect(assocac.MaxOperationsPerformed).To(Equal(uint16(0)))
-
-	Expect(assocac.ImplementationVersion).To(Equal("dcm4che-2.0"))
+	if !reflect.DeepEqual(exp, got) {
+		t.Fatalf("expected\n%+V\ngot\n%+V", exp, got)
+	}
 }
 
-func readAssocRQAC(file string, pduType PDUType) AssociateRQAC {
+func readAssocRQAC(t *testing.T, file string, pduType PDUType) AssociateRQAC {
 	b, err := ioutil.ReadFile(file)
-	Expect(err).To(BeNil())
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	var buf bytes.Buffer
 	buf.Write(b)
@@ -74,15 +77,23 @@ func readAssocRQAC(file string, pduType PDUType) AssociateRQAC {
 
 	pduDecoder := NewPDUDecoder(&buf)
 	pdu, err := pduDecoder.NextPDU()
-	Expect(err).To(BeNil())
-	Expect(pdu).ToNot(BeNil())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pdu == nil {
+		t.Fatal("no pdu found")
+	}
 
-	Expect(pdu.Type).To(Equal(pduType))
+	if pduType != pdu.Type {
+		t.Fatalf("unexpected pdu type, expected %s, got %s",
+			pduType, pdu.Type)
+	}
 	var rqac AssociateRQAC
 	rqac.Read(pdu.Data)
 
-	// ensure that we stopped at the right spot:
-	Expect(buf.String()).To(Equal("don't read me bro"))
+	if "don't read me bro" != buf.String() {
+		t.Fatal("didn't stop reading at the right place")
+	}
 
 	return rqac
 }
