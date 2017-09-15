bba14b9153cee82975f25f63014563e24e565582
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2017-09-15 08:28:16 +0200

    avoid ordering dependency among tests

--- dcmnet/pc_test.go
+++ dcmnet/pc_test.go
@@ -30,22 +30,24 @@ func TestRole(t *testing.T) {
 }
 
 // simple case with single, matching, accepted contexts
-var simplePcs = PresentationContexts{
-	Requested: []PresentationContext{{
-		AbstractSyntax: "1.2.3",
-		ID:             1,
-		TransferSyntaxes: []dcm.TransferSyntax{
-			dcm.ImplicitVRLittleEndian,
-		},
-	}},
-	Accepted: []PresentationContext{{
-		AbstractSyntax: "1.2.3",
-		ID:             1,
-		Result:         PCAcceptance,
-		TransferSyntaxes: []dcm.TransferSyntax{
-			dcm.ImplicitVRLittleEndian,
-		},
-	}},
+func simplePcs() PresentationContexts {
+	return PresentationContexts{
+		Requested: []PresentationContext{{
+			AbstractSyntax: "1.2.3",
+			ID:             1,
+			TransferSyntaxes: []dcm.TransferSyntax{
+				dcm.ImplicitVRLittleEndian,
+			},
+		}},
+		Accepted: []PresentationContext{{
+			AbstractSyntax: "1.2.3",
+			ID:             1,
+			Result:         PCAcceptance,
+			TransferSyntaxes: []dcm.TransferSyntax{
+				dcm.ImplicitVRLittleEndian,
+			},
+		}},
+	}
 }
 
 // matches simplePcs above
@@ -58,29 +60,29 @@ func expectMatchingTc(t *testing.T, got *TransferCapability) {
 }
 
 func TestPCsSingleAcceptance(t *testing.T) {
-	if got := simplePcs.FindAcceptedTCap(PCID(1)); !reflect.DeepEqual(&matchingTc, got) {
+	pcs := simplePcs()
+	if got := pcs.FindAcceptedTCap(PCID(1)); !reflect.DeepEqual(&matchingTc, got) {
 		t.Errorf("unknown tc for pcid 1: %s", got)
 	}
-	expectMatchingTc(t, simplePcs.FindAcceptedTCap(PCID(1)))
+	expectMatchingTc(t, pcs.FindAcceptedTCap(PCID(1)))
 
-	if got := simplePcs.FindAcceptedTCap(PCID(2)); got != nil {
+	if got := pcs.FindAcceptedTCap(PCID(2)); got != nil {
 		t.Errorf("expected no tc for pcid 2 but got: %s", got)
 	}
 
-	if got := simplePcs.FindAcceptedPCID(matchingTc); got == nil || PCID(1) != *got {
+	if got := pcs.FindAcceptedPCID(matchingTc); got == nil || PCID(1) != *got {
 		t.Errorf("unexpected pcid: %d", got)
 	}
 
 	nonMatchingTc := matchingTc
 	nonMatchingTc.AbstractSyntax = "1.2.3.4"
-	if got := simplePcs.FindAcceptedPCID(nonMatchingTc); got != nil {
+	if got := pcs.FindAcceptedPCID(nonMatchingTc); got != nil {
 		t.Errorf("expected no pcid but got: %d", got)
 	}
 }
 
 func TestPCsMultipleProposedTransferSyntaxes(t *testing.T) {
-	pcs := simplePcs
-	// No! This modifies the global!
+	pcs := simplePcs()
 	pcs.Requested[0].TransferSyntaxes = []dcm.TransferSyntax{
 		// add a new one first, make sure it gets skipped:
 		dcm.ExplicitVRLittleEndian,
@@ -91,7 +93,7 @@ func TestPCsMultipleProposedTransferSyntaxes(t *testing.T) {
 }
 
 func TestPCsNoOverlappingTransferSyntaxes(t *testing.T) {
-	pcs := simplePcs
+	pcs := simplePcs()
 	// this probably isn't technically legal in dicom, but the code should
 	// handle it:
 	pcs.Accepted[0].TransferSyntaxes = []dcm.TransferSyntax{
@@ -104,7 +106,7 @@ func TestPCsNoOverlappingTransferSyntaxes(t *testing.T) {
 }
 
 func TestPCsSingleUnacceptedContext(t *testing.T) {
-	pcs := simplePcs
+	pcs := simplePcs()
 	pcs.Accepted[0].Result = PCUserRejection
 
 	if got := pcs.FindAcceptedTCap(PCID(1)); got != nil {
