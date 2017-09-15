334e210448396b8ca423b07e0745adc1708d3091
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2017-09-15 08:20:52 +0200

    less gomega

--- dcmnet/pc_test.go
+++ dcmnet/pc_test.go
@@ -1,89 +1,96 @@
 package dcmnet
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/jeremyhuiskamp/dcm/dcm"
-	. "github.com/onsi/gomega"
 )
 
 func TestRole(t *testing.T) {
-	RegisterTestingT(t)
+	expectRole := func(r Role, scu, scp bool) {
+		if r.IsSCU() != scu {
+			t.Errorf("%s expected scu %t", r, scu)
+		}
+		if r.IsSCP() != scp {
+			t.Errorf("%s expected scp %t", r, scp)
+		}
+	}
 
-	Expect(DefaultRole.IsSCU()).To(BeTrue())
-	Expect(DefaultRole.IsSCP()).To(BeFalse())
+	expectRole(DefaultRole, true, false)
 
 	var role Role
-
-	Expect(role.IsSCU()).To(BeFalse())
-	Expect(role.IsSCP()).To(BeFalse())
+	expectRole(role, false, false)
 
 	role.SetSCP(true)
-	Expect(role.IsSCU()).To(BeFalse())
-	Expect(role.IsSCP()).To(BeTrue())
+	expectRole(role, false, true)
 
 	role.SetSCU(true)
-	Expect(role.IsSCU()).To(BeTrue())
-	Expect(role.IsSCP()).To(BeTrue())
+	expectRole(role, true, true)
 }
 
 // simple case with single, matching, accepted contexts
 var simplePcs = PresentationContexts{
-	Requested: []PresentationContext{
-		{
-			AbstractSyntax: "1.2.3",
-			ID:             1,
-			TransferSyntaxes: []dcm.TransferSyntax{
-				dcm.ImplicitVRLittleEndian,
-			},
+	Requested: []PresentationContext{{
+		AbstractSyntax: "1.2.3",
+		ID:             1,
+		TransferSyntaxes: []dcm.TransferSyntax{
+			dcm.ImplicitVRLittleEndian,
 		},
-	},
-	Accepted: []PresentationContext{
-		{
-			AbstractSyntax: "1.2.3",
-			ID:             1,
-			Result:         PCAcceptance,
-			TransferSyntaxes: []dcm.TransferSyntax{
-				dcm.ImplicitVRLittleEndian,
-			},
+	}},
+	Accepted: []PresentationContext{{
+		AbstractSyntax: "1.2.3",
+		ID:             1,
+		Result:         PCAcceptance,
+		TransferSyntaxes: []dcm.TransferSyntax{
+			dcm.ImplicitVRLittleEndian,
 		},
-	},
+	}},
 }
 
 // matches simplePcs above
 var matchingTc = NewTransferCapability("1.2.3", dcm.ImplicitVRLittleEndian)
 
+func expectMatchingTc(t *testing.T, got *TransferCapability) {
+	if !reflect.DeepEqual(&matchingTc, got) {
+		t.Errorf("unexpected transfer capability: %s", got)
+	}
+}
+
 func TestPCsSingleAcceptance(t *testing.T) {
-	RegisterTestingT(t)
+	if got := simplePcs.FindAcceptedTCap(PCID(1)); !reflect.DeepEqual(&matchingTc, got) {
+		t.Errorf("unknown tc for pcid 1: %s", got)
+	}
+	expectMatchingTc(t, simplePcs.FindAcceptedTCap(PCID(1)))
 
-	Expect(simplePcs.FindAcceptedTCap(PCID(1))).To(Equal(&matchingTc))
+	if got := simplePcs.FindAcceptedTCap(PCID(2)); got != nil {
+		t.Errorf("expected no tc for pcid 2 but got: %s", got)
+	}
 
-	Expect(simplePcs.FindAcceptedTCap(PCID(2))).To(BeNil())
-
-	pcid1 := PCID(1)
-	Expect(simplePcs.FindAcceptedPCID(matchingTc)).To(Equal(&pcid1))
+	if got := simplePcs.FindAcceptedPCID(matchingTc); got == nil || PCID(1) != *got {
+		t.Errorf("unexpected pcid: %d", got)
+	}
 
 	nonMatchingTc := matchingTc
 	nonMatchingTc.AbstractSyntax = "1.2.3.4"
-	Expect(simplePcs.FindAcceptedPCID(nonMatchingTc)).To(BeNil())
+	if got := simplePcs.FindAcceptedPCID(nonMatchingTc); got != nil {
+		t.Errorf("expected no pcid but got: %d", got)
+	}
 }
 
 func TestPCsMultipleProposedTransferSyntaxes(t *testing.T) {
-	RegisterTestingT(t)
-
 	pcs := simplePcs
+	// No! This modifies the global!
 	pcs.Requested[0].TransferSyntaxes = []dcm.TransferSyntax{
 		// add a new one first, make sure it gets skipped:
 		dcm.ExplicitVRLittleEndian,
 		pcs.Requested[0].TransferSyntaxes[0],
 	}
 
-	Expect(pcs.FindAcceptedTCap(PCID(1))).To(Equal(&matchingTc))
+	expectMatchingTc(t, pcs.FindAcceptedTCap(PCID(1)))
 }
 
 func TestPCsNoOverlappingTransferSyntaxes(t *testing.T) {
-	RegisterTestingT(t)
-
 	pcs := simplePcs
 	// this probably isn't technically legal in dicom, but the code should
 	// handle it:
@@ -91,15 +98,19 @@ func TestPCsNoOverlappingTransferSyntaxes(t *testing.T) {
 		dcm.ExplicitVRBigEndian,
 	}
 
-	Expect(pcs.FindAcceptedPCID(matchingTc)).To(BeNil())
+	if got := pcs.FindAcceptedPCID(matchingTc); got != nil {
+		t.Errorf("expected no pcid but got: %d", got)
+	}
 }
 
 func TestPCsSingleUnacceptedContext(t *testing.T) {
-	RegisterTestingT(t)
-
 	pcs := simplePcs
 	pcs.Accepted[0].Result = PCUserRejection
 
-	Expect(pcs.FindAcceptedTCap(PCID(1))).To(BeNil())
-	Expect(pcs.FindAcceptedPCID(matchingTc)).To(BeNil())
+	if got := pcs.FindAcceptedTCap(PCID(1)); got != nil {
+		t.Errorf("expected no tcap but got: %d", got)
+	}
+	if got := pcs.FindAcceptedPCID(matchingTc); got != nil {
+		t.Errorf("expected no pcid but got: %d", got)
+	}
 }
